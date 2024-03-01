2ba7451a0bf130d6a66b96ffdf02148e5cfa65fd
Dayer <93074807+dayercher005@users.noreply.github.com>
2024-03-02 00:01:44 +0800

    Add files via upload

---
+++ Javascript (Functions)/script.js
@@ -0,0 +1,18 @@
+
+
+function add7 (number){
+    return num + 7
+}
+
+function multiply (number1, number2){
+    return number1 * number2
+}
+
+function capitalize (word){
+    uppercase(word)
+}
+
+function lastLetter (another_word) {
+   let res = another_word.at(-1)
+   return res
+}
